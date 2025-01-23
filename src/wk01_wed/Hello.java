cc70041065106834a45868e10ab54cf064d0e41e
sislam <sislam@csumb.edu>
2025-01-22 16:12:44 -0800

    Wk01 Wednesday

---
+++ src/wk01_wed/Hello.java
@@ -0,0 +1,27 @@
+package wk01_wed;
+
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello CST 231 Class!");
+        int x;
+        x = 5;
+        int y = 7;
+        System.out.println(x);
+        int z = x + y;
+        System.out.println(z);
+        double a = 7.5;
+        System.out.println(a);
+        double b = a + x;
+        System.out.println(b);
+        b = a * 2;
+        System.out.println("Res: " + b);
+
+        x = 13/3;
+        System.out.println(x);
+        b = 13.0/3;
+        System.out.println(b);
+
+        x = 13%3;
+        System.out.println(x);
+    }
+}
