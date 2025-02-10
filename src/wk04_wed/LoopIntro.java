f2e24651a218ed1ae394fe36f6fc999d5a5fb14f
sislam <sislam@csumb.edu>
2025-02-10 15:07:49 -0800

    Wk04 Monday

---
+++ src/wk04_wed/LoopIntro.java
@@ -0,0 +1,27 @@
+package wk04_wed;
+
+import java.util.Scanner;
+
+public class LoopIntro {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int num = 0;
+        int sum = 0;
+        while (num >= 0){
+            sum += num;
+            System.out.println("Sum: " + sum);
+            System.out.print("Enter a number: ");
+            num = in.nextInt();
+        }
+        System.out.println("Final Sum: " + sum);
+
+        int i = 10;
+        while(i >= 0){
+            System.out.print(i + " ");
+//            i -= 2; // Same as i = i - 2;
+            i--; // Same as i = i - 1;
+        }
+        System.out.println("\nDone with the loop");
+
+    }
+}
